f92e20c42b3f9dfbd131c142abea3e8edc7666f7
Derin <tekinderin5@gmail.com>
2023-02-19 22:43:59 -0500

    commit

--- scientificOperations.py
+++ scientificOperations.py
@@ -13,23 +13,21 @@ def scientific_calculate(operation, num1):
         - the result of the operation on the given number
     """
 
+        #my work
     if operation == 'sin':
         return math.sin(num1)
 
     elif operation == 'cos':
-        # TODO: finish this statement
-        # - should return the cosine of num1
-        pass  # Replace this line with your code
+        return math.cos(num1)
+        
 
     elif operation == 'tan':
-        # TODO: finish this statement
-        # - should return the tangent of num1
-        pass  # Replace this line with your code
+        return math.tan(num1)
+        
 
     elif operation == 'ln':
-        # TODO: finish this statement
-        # - should return the natural log of num1
-        pass  # Replace this line with your code
+        return math.ln(num1)
+        
 
     else:
         raise ValueError(
--- simpleOperations.py
+++ simpleOperations.py
@@ -10,25 +10,19 @@ def simple_calculate(operation, num1, num2):
     returns:
         - the result of the operation on the given numbers
     """
-
+#my work
     if operation == '+':
         return num1 + num2
 
     elif operation == '-':
-        # TODO: finish this statement
-        # - should return the result of subtracting num2 from num1
-        pass  # Replace this line with your code
-
+        return num1 - num2
+        
     elif operation == '*':
-        # TODO: finish this statement
-        # - should return the result of multiplying num1 by num2
-        pass  # Replace this line with your code
-
+        return num1 * num2
+        
     elif operation == '/':
-        # TODO: finish this statement
-        # - should return the result of dividing num1 by num2
-        # - remember to raise a ValueError if num2 is 0
-        pass  # Replace this line with your code
+        return num1/num2
+        
     else:
         raise ValueError(
-            'Invalid Operation: Simple Operations are (+, -, *, and /)')
+            'Invalid Operation: The Simple Operations are (+, -, *, and /)')
